6973dfee12d548960a4995b56a955d813f3a1454
polin103109 <sabrinpolinece18@gmail.com>
2024-02-05 01:31:59 +0600

    added sections with css and js

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-const nav = document.querySelector('.nav-background');
-const toggler = document.querySelector('.menu__toggler');
-toggler.addEventListener('click', () => {
-  toggler.classList.toggle('active');
-  nav.classList.toggle('active');
-})
+
+document.addEventListener('DOMContentLoaded', function () {
+  const checkbox = document.getElementById('nav-toggle');
+  const navigationButton = document.querySelector('.navigation__button');
+  const navigationNav = document.querySelector('.navigation__nav');
+ // const navigationBackground = document.querySelector('.navigation__background');
+
+  navigationButton.addEventListener('click', function () {
+      checkbox.checked = !checkbox.checked;
+      updateNavigationState();
+  });
+
+  // Update navigation state on checkbox change
+  checkbox.addEventListener('change', function () {
+      updateNavigationState();
+  });
+
+  function updateNavigationState() {
+      const isOpen = checkbox.checked;
+      document.body.classList.toggle('nav-open', isOpen);
+      navigationNav.style.opacity = isOpen ? 1 : 0;
+      navigationNav.style.width = isOpen ? '100%' : '0';
+     // navigationBackground.style.transform = isOpen ? 'scale(80)' : 'scale(0)';
+  }
+});
