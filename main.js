1eadb827ff5804d4ca4979ea1e2411efae754bb1
polin103109 <sabrinpolinece18@gmail.com>
2024-02-06 17:22:38 +0600

    added the responsiveness

--- main.js
+++ main.js
@@ -3,8 +3,16 @@ document.addEventListener('DOMContentLoaded', function () {
   const checkbox = document.getElementById('nav-toggle');
   const navigationButton = document.querySelector('.navigation__button');
   const navigationNav = document.querySelector('.navigation__nav');
- // const navigationBackground = document.querySelector('.navigation__background');
-
+  const navigationBackground = document.querySelector('.navigation__background');
+  const navToggle = document.querySelector('.navigation__checkbox');
+  const navIcon = document.querySelector('.navigation__icon');
+  navToggle.addEventListener('change', function() {
+    if (navToggle.checked) {
+      navIcon.classList.add('rotate');
+    } else {
+      navIcon.classList.remove('rotate');
+    }
+  });
   navigationButton.addEventListener('click', function () {
       checkbox.checked = !checkbox.checked;
       updateNavigationState();
@@ -17,9 +25,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function updateNavigationState() {
       const isOpen = checkbox.checked;
+      console.log(isOpen)
       document.body.classList.toggle('nav-open', isOpen);
       navigationNav.style.opacity = isOpen ? 1 : 0;
       navigationNav.style.width = isOpen ? '100%' : '0';
-     // navigationBackground.style.transform = isOpen ? 'scale(80)' : 'scale(0)';
+      navigationBackground.style.transform = isOpen ? 'scale(80)' : 'scale(0)';
   }
 });
